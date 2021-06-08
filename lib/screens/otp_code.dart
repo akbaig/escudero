90bce713ae7622ab2ba1d6b51d09a1f41f229f72
akbaig <57063370+akbaig@users.noreply.github.com>
2021-06-08 14:38:42 +0500

    completed otp screen

--- lib/screens/otp_code.dart
+++ lib/screens/otp_code.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -18,25 +19,114 @@ class _OTPState extends State<OTP> {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            DigitFormField(
-              focusNode: node,
-              initialFocus: true,
-              text: "1",
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Text.rich(
+                TextSpan(
+                  text: "Please Enter 6 - Digit Verification Code sent by SMS on your\nMobile Number ",
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                      text: '(+1)23 456 789',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                      )
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ) 
             ),
-            DigitFormField(
-              focusNode: node,
-              text: "2",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: List.generate(6, (int index) => 
+                DigitFormField(
+                  focusNode: node,
+                  text: "${index+1}",
+              )), 
             ),
-            DigitFormField(
-              focusNode: node,
-              text: "3",
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width/1.5,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.orange.shade700,
+                        Colors.orange.shade900,
+                      ],
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Next", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            DigitFormField(
-              focusNode: node,
-              text: "4",
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Text.rich(
+                TextSpan(
+                  text: "Didn't get OTP? ",
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                      text: 'Resend OTP',
+                      style: TextStyle(
+                        color: Colors.orange.shade900,
+                        decoration: TextDecoration.underline,
+                      )
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ) 
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Text("2:19",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 30.0),
+              child: GestureDetector(
+                onTap: () { },
+                child: Text("Skip Login",
+                  style: TextStyle(
+                    color: Colors.orange.shade900,
+                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
             ),
           ],
         ),
@@ -47,11 +137,10 @@ class _OTPState extends State<OTP> {
 
 
 class DigitFormField extends StatefulWidget {
-  final bool initialFocus;
   final int flex; 
   final String text;
   final FocusNode focusNode;
-  DigitFormField({this.text, this.flex = 1, this.focusNode, this.initialFocus = false});
+  DigitFormField({this.text, this.flex = 1, this.focusNode});
   @override
   _DigitFormFieldState createState() => _DigitFormFieldState();
 }
@@ -76,7 +165,7 @@ class _DigitFormFieldState extends State<DigitFormField> {
     return Expanded(
       flex: widget.flex,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: TextFormField(
           textAlign: TextAlign.center,
           controller: controller,
@@ -89,6 +178,11 @@ class _DigitFormFieldState extends State<DigitFormField> {
             hintStyle: TextStyle(
               color: Colors.grey,
             )
+          ),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 14.0,
           ),
           onChanged: (String val) {
             if(val.length == 1)
